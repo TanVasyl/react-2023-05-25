ebfb6b6fd9c4b8d0844d24b0a76995f26b91d6e0
TanVasyl <99660402+TanVasyl@users.noreply.github.com>
2023-06-11 21:31:59 +0300

    add rating button
    add rating button and disabled button

--- src/components/Rating/Rating.jsx
+++ src/components/Rating/Rating.jsx
@@ -1,5 +1,16 @@
 import React from "react";
 
 export const Rating = ({ value, onChange }) => {
-  return <div>Rating</div>;
+  const rating = [1, 2, 3, 4, 5];
+  return (
+    <div>
+      {rating.map((num) => {
+        return num === value ? (
+          <button disabled>{num}</button>
+        ) : (
+          <button onClick={() => onChange(num)}>{num}</button>
+        );
+      })}
+    </div>
+  );
 };
