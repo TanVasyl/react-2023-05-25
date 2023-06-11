e5b55a175d47b710fcc1c8cbdcf1078707a2264d
TanVasyl <99660402+TanVasyl@users.noreply.github.com>
2023-06-11 21:27:42 +0300

    update Dish component
    add counter, add display of ingredients

--- src/components/Dish/Dish.jsx
+++ src/components/Dish/Dish.jsx
@@ -4,12 +4,34 @@ export const Dish = ({ dish }) => {
   if (!dish) {
     return null;
   }
+  const { name, price, ingredients } = dish;
+  const [count, setCount] = React.useState(0);
+  const increment = () => {
+    count >= 5 ? 5 : setCount(count + 1);
+  };
+  const decrement = () => {
+    count === 0 ? 0 : setCount(count - 1);
+  };
 
-  const { name, price } = dish;
   return (
     <div>
       <p>{name}</p>
       <p>{price}</p>
+      <p>
+        <button onClick={increment}>+</button>
+        <span>{count}</span>
+        <button onClick={decrement}>-</button>
+      </p>
+      {count > 0 ? (
+        <p>
+          Состав:
+          {ingredients.map((item) => {
+            return <span>{item} </span>;
+          })}
+        </p>
+      ) : (
+        <p></p>
+      )}
     </div>
   );
 };
